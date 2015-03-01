8136f691eb3919fe7b66fe733f58b6590e1b4917
tnt <tnt@banza.net>
2015-03-01 21:22:08 +0100

    improved API and completely reorganized and improved code and behavior

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # ansible-gpg-keys-mod
 
-Not yet fully perfect functioning Ansible module to manage GPG-keys.
+Ansible module to manage GPG-keys.
+
+It addresses the issues of non-responding keyservers in general by repeating attempts and keys.gnupg.net's round-robin-DNS sabotaging DNS-caching (like the Windows DNS-cache does it) in particular by optionally trying alternating hostnames.
 
 ### examples
 
 ```YAML
 tasks:
   - name: Install GPG key
-    gpg_keys_mod: key_id="0x3804BB82D39DC0E3"
+    gpg_keys_mod: key_id="0x3804BB82D39DC0E3" state=present
 ```
 or
 ```YAML
 tasks:
-  - name: Install GPG key
+  - name: Install or update GPG key
     gpg_keys_mod:
       key_id: "0x3804BB82D39DC0E3"
+      state: latest
       servers:
         - 'hkp://no.way.ever'
         - 'keys.gnupg.net'
@@ -26,6 +29,7 @@ tasks:
   - name: Install or fail with fake and not fake GPG keys
     gpg_keys_mod:
       key_id: "{{ item }}"
+      state: present
       tries: 2
       delay: 0
     with_items:
@@ -37,10 +41,10 @@ tasks:
 name         | default            | description
 -------------|:------------------:|-------------
 servers      | [ keys.gnupg.net ] | list of hostnames (or `hkp://`/`hkps://` urls) to try
-tries        |   3                | number of attempts per server
-delay        |  0.5               |
-refresh      | no                 | calls `gpg --refresh-keys ...` - always results in *changed* state
-delete       | no                 | calls `gpg --delete-keys ...`
+tries        |   3                | number of attempts per *server*
+delay        |  0.5               | delay between retries
+state        | 'present'          | desired state 'present', 'latest', 'refreshed' or 'absent' ('refreshed' == 'latest')
+gpg_timeout  | 5                  | `gpg --keyserver-options timeout=5 ...`
 
 
-[Strange behaviors](https://gist.github.com/tnt/eedaed9a6cc75130b9cb) occurs when used with [insane keys](https://gist.github.com/tnt/70b116c72be11dc3cc66). But this is a gpg-problem.
+[Strange behaviors](https://gist.github.com/tnt/eedaed9a6cc75130b9cb) occur when used with [insane keys](https://gist.github.com/tnt/70b116c72be11dc3cc66). But this is a gpg-problem.
--- gpg_keys_mod.py
+++ gpg_keys_mod.py
@@ -8,47 +8,70 @@ class GpgKeysMod(object):
 
     def __init__(self, module):
         self.m = module
-        self.setup_creds()
-        self.set_urls()
+        self._setup_creds()
+        self._execute_task()
 
-    def set_urls(self):
+    def _execute_task(self):
+        res = self._execute_command('check')
+        key_present = res['rc'] == 0
+
+        if key_present and self.state == 'absent':
+            res = self._execute_command('delete')
+            self.changed = res['rc'] == 0
+        elif key_present and self.state in ('latest','refreshed'):
+            res = self._repeat_command('refresh')
+            self.changed = re.search('gpg:\s+unchanged: 1\n$', res['stderr']) is None
+        elif not key_present and self.state in ('present','latest','refreshed'):
+            res = self._repeat_command('recv')
+            self.changed = res['rc'] == 0
+        else:
+            self.changed = False
+
+        if res['rc'] != 0:
+            self.m.fail_json(msg=self.log_dic)
+
+
+    def _setup_creds(self):
+        for k,v in self.m.params.items():
+            setattr(self, k, v)
+        self.commands = {
+            'check':   '%s %s --list-keys %%s',
+            'delete':  '%s %s --batch --yes --delete-keys %%s',
+            'refresh': '%s %s --keyserver %%s --keyserver-options timeout=%%d --refresh-keys %%s',
+            'recv':    '%s %s --keyserver %%s --keyserver-options timeout=%%d --recv-keys %%s'
+        }
+        bp = self.m.get_bin_path('gpg', True)
+        check_mode = '--dry-run' if self.m.check_mode else ''
+        for c,l in self.commands.items():
+            self.commands[c] = l % (bp, check_mode)
         self.urls = [s if re.match('hkps?://', s)
                        else 'hkp://%s' % s
                      for s in self.servers]
 
-    def setup_creds(self):
-        for k,v in self.m.params.items():
-            setattr(self, k, v)
-        self.commands = {
-            'check':   '%s --list-keys %%s',
-            'delete':  '%s --batch --yes --delete-keys %%s',
-            'refresh': '%s --keyserver %%s --refresh-keys %%s',
-            'recv':    '%s --keyserver %%s --recv-keys %%s'
-        }
-        bp = self.m.get_bin_path('gpg', True)
-        for c,l in self.commands.items():
-            self.commands[c] = l % bp
-        self.c_results = dict([k, {'tries': [], 'num_tries':  0}] for k in self.commands)
-
-    def repeat_command(self, cmd):
+    def _repeat_command(self, cmd):
         for n in range(self.tries):
             for u in self.urls:
-                res = self.m.run_command(self.commands[cmd] % (u, self.key_id))
-                self._to_c_results(cmd, res)
+                args = (u, self.gpg_timeout, self.key_id)
+                raw_res = self.m.run_command(self.commands[cmd] % args)
+                res = self._legiblify(cmd, raw_res)
+                if res['rc'] == 0:
+                    return res
                 sleep(self.delay)
-                if res[0] == 0:
-                    return 0
-        return 8888
+        return {'rc': 8888}
 
-    def execute_command(self, cmd):
-        res = self.m.run_command(self.commands[cmd] % self.key_id)
-        self._to_c_results(cmd, res)
-        return res[0]
+    def _execute_command(self, cmd):
+        raw_res = self.m.run_command(self.commands[cmd] % self.key_id)
+        return self._legiblify(cmd, raw_res)
 
-    def _to_c_results(self, sec, res):
+    def _legiblify(self, sec, res):
+        """turn tuple to dict and preserve it for debugging"""
+        if not hasattr(self, 'log_dic'):
+            self.log_dic = {}
         rdic = dict([k, res[i]] for i,k in enumerate(('rc', 'stdout', 'stderr')))
-        self.c_results[sec]['tries'].append(rdic)
-        self.c_results[sec]['num_tries'] += 1
+        self.log_dic.setdefault(sec, {'tries': [], 'num_tries':  0})
+        self.log_dic[sec]['tries'].append(rdic)
+        self.log_dic[sec]['num_tries'] += 1
+        return rdic
 
 
 def main():
@@ -58,45 +81,16 @@ def main():
             servers=dict(default=['keys.gnupg.org'], type='list'),
             tries=dict(default=3, type='int'),
             delay=dict(default=0.5),
-            refresh=dict(default=False, type='bool'),
-            delete=dict(default=False, type='bool')
+            state=dict(default='present', choices=['latest', 'refreshed', 'absent', 'present']),
+            gpg_timeout=dict(default=5, type='int')
         ),
         supports_check_mode=True
     )
 
     gkm = GpgKeysMod(module)
 
-    if gkm.refresh and gkm.delete:
-        module.fail_json(msg='delete and refresh are exclusive')
-
-    key_present = gkm.execute_command('check') == 0
-
-    delete_present = key_present and gkm.delete
-    refresh = key_present and gkm.refresh
-    fetch_missing = not key_present and not gkm.delete
-
-    changed = False
-    if module.check_mode and (delete_present or refresh or fetch_missing):
-        changed = True
-    else:
-        meth, cmd = lambda x: None, 'placebo'    # yet improvable
-
-        if delete_present:
-            meth, cmd = gkm.execute_command, 'delete'
-        elif refresh:
-            meth, cmd = gkm.repeat_command, 'refresh'
-        elif fetch_missing:
-            meth, cmd = gkm.repeat_command, 'recv'
-
-        rc = meth(cmd)
-
-        if rc == 0:
-            changed = True
-        elif rc is not None:
-            module.fail_json(msg=gkm.c_results[cmd])
-
-    result = {'c_results': gkm.c_results,
-              'changed': changed}
+    result = {'log_dic': gkm.log_dic,
+              'changed': gkm.changed}
 
     module.exit_json(**result)
 
